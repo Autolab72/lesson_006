cf2ec86375bfdfc1260c35021dae9be71317f4b8
Vitaliy <71567377+Autolab72@users.noreply.github.com>
2021-10-26 01:07:03 +0500

    mastermind_engine.py in progress

--- mastermind_engine.py
+++ mastermind_engine.py
@@ -2,11 +2,17 @@ from random import randint
 
 hidden_number = []
 user_numbers = []
+def get_hidden_number():
+    print("Число загаданно, приступайте...")
+    for i in range(4):
+        hidden_number.append(randint(1, 9))
+
+
 def get_number():
     global user_numbers
     user_numbers = []
     for i in range(len(hidden_number)):
-        input_number = input(f"Введите {i}е число от 1 до 9: ")
+        input_number = int(input(f"Введите {i}е число от 1 до 9: "))
         if input_number < 0 :
             print("Это число меньше 0, введите целое число от 1 до 9")
             continue
@@ -14,7 +20,9 @@ def get_number():
             print("Это число больше 9 введите целое число от 1 до 9")
             continue
         else:
-            user_numbers.append(input_number)
+            user_numbers.append(i)
 
-get_number()
-# print(user_numbers)
+
+def check_number():
+    pass
+
